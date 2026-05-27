073fa876b8457df85450182657a38b4eb5c0c2c6
Tanfeez304204 <naushadiqbal813@gmail.com>
2026-05-27 12:09:51 +0530

    adsense

--- app/layout.jsx
+++ app/layout.jsx
@@ -25,6 +25,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-N638RNLFR1"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-N638RNLFR1');
+            `
+          }}
+        />
         {/* Google AdSense Script */}
         <script
           async
